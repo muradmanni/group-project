63d7ee1b8cd2ccfb9b7a3091bca6fc771e91c2a2
Murad Ali <murad.manni@gmail.com>
2022-03-30 00:50:39 +1100

    movie search data retrieved, checking if true or false

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var sectionSearch = document.querySelector('#section-search');
 var textboxSearch = document.querySelector('#textbox-search');
 
 var omdbApiKey="ef78856e";
-var omdbUrl ="https://www.omdbapi.com/?apikey=" + omdbApiKey + "&type=movie&page=2&s=";
+var omdbUrl ="https://www.omdbapi.com/?apikey=" + omdbApiKey + "&type=movie&s=";
 var omdb1 = "https://www.omdbapi.com/?apikey=" + omdbApiKey + "&i=tt1285016"
 btnSearch.addEventListener('click',searchMovie);
 textboxSearch.addEventListener('keyup', toggleSearchButton);
@@ -14,20 +14,29 @@ function toggleSearchButton(){
 
 function searchMovie(event){
     event.preventDefault();
-    var omdb = omdbSearch(textboxSearch.value);
+    omdbSearch(textboxSearch.value);
 
     sectionSearch.setAttribute('class','hero');
 }
 
 function omdbSearch(movieTitle){
-    fetch(omdb1).then(function (response) {
+    fetch(omdbUrl + movieTitle).then(function (response) {
         if (response.ok) {
           response.json().then(function (data) {
             console.log(data);
-    
+            if (data['Response']==='False'){
+                console.log("Not Found");
+                Bulma().alert({
+                    type: 'danger',
+                    title: 'This is an alert!',
+                    body: 'Ooohh what button you gonna click?',
+                    confirm: 'Confirm it!',
+                    cancel: 'Maybe not'
+                });
+            }
           });
         } else {
-          return "error";
+            
         }
       });
     
