f8166c402c2853755370739ca43fc0eaf8f0645f
Murad Ali <murad.manni@gmail.com>
2022-03-30 10:43:34 +1100

    layout for movie search display completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-var btnSearch = document.querySelector('#btn-search');
-var sectionSearch = document.querySelector('#section-search');
-var textboxSearch = document.querySelector('#textbox-search');
+
+var btnSearch = document.querySelector("#btn-search");
+var sectionSearch = document.querySelector("#section-search");
+var textboxSearch = document.querySelector("#textbox-search");
 
 var omdbApiKey="ef78856e";
 var omdbUrl ="https://www.omdbapi.com/?apikey=" + omdbApiKey + "&type=movie&s=";
-var omdb1 = "https://www.omdbapi.com/?apikey=" + omdbApiKey + "&i=tt1285016"
-btnSearch.addEventListener('click',searchMovie);
-textboxSearch.addEventListener('keyup', toggleSearchButton);
+var omdbSingleSearchUrl = "https://www.omdbapi.com/?apikey=" + omdbApiKey + "&i="
+btnSearch.addEventListener("click",searchMovie);
+textboxSearch.addEventListener("keyup", toggleSearchButton);
 
 var pageNumber;
 var totalPages;
@@ -18,37 +19,103 @@ function toggleSearchButton(){
 
 function searchMovie(event){
     event.preventDefault();
-    omdbSearch(textboxSearch.value,2);
+    omdbSearchTitle(textboxSearch.value,1);
 
     
-    sectionSearch.setAttribute('class','hero');
+    sectionSearch.setAttribute("class","hero");
 }
 
-function omdbSearch(movieTitle,page){
+function omdbSearchTitle(movieTitle,page){
     console.log(omdbUrl + movieTitle + "&page=" + page);
     fetch(omdbUrl + movieTitle + "&page=" + page).then(function (response) {
         if (response.ok) {
           response.json().then(function (data) {
-            if (data['Response']==='False'){
+            if (data["Response"]==="False"){
                 console.log("Not Found");
+                //CHANGE console log to MODAL display
             }
             else{
                 showSearchResult(data);
             }
           });
         } else {
-            
+            //if response not ok have to show error in modal
         }
       });
-    
+}
+
+function omdbGetSingleMovieDetails(omdbid){
+
+    console.log(omdbSingleSearchUrl  + omdbid);
+    fetch(omdbSingleSearchUrl+ omdbid).then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            if (data["Response"]==="False"){
+                console.log("Not Found");
+                //CHANGE console log to MODAL display
+            }
+            else{
+                console.log(data);
+            }
+          });
+        } else {
+            //if response not ok have to show error in modal
+        }
+      });
 }
 
 function showSearchResult(data){
+    document.body.removeChild(document.body.lastChild);
     console.log(data);
-    totalMovies=data['totalResults'];
+    totalMovies=data["totalResults"];
     calculateTotalPages(totalMovies);
+    console.log("total movies in this page is " + data['Search'].length)
+
+    var sectionMovieResult=document.createElement("section");
+
+    sectionMovieResult.className= "hero display-result center-please";
+    sectionMovieResult.setAttribute("id","section-display-result")
+    
+    var searchMovieDivContainer=document.createElement ("div");
+    searchMovieDivContainer.className= "container is-fluid";
+    var searchMovieDivContainerColumn = document.createElement("div");
+    searchMovieDivContainerColumn.className = "columns is-multiline is-centered";
+        
+    for (var i=0; i< data["Search"].length; i++)
+    {
+        // -------------------- GET VALUES --------------------
+        var poster = (data['Search'][i]['Poster']);
+        var movieTitle =(data['Search'][i]['Title']);
+        if (poster==="N/A")
+        {
+            poster="./assets/images/image-not-available.jpg";
+        }
+    
 
 
+
+        // ------------------- GENERATE RESULT AND SHOW -------------------
+        var divOutBox = document.createElement("div");
+        divOutBox.className="column is-3-fullhd  is-3-desktop is-6-tablet is-12-mobile";
+        var divInsideBox = document.createElement("div");
+        divInsideBox.className="notification is-primary has-text-centered";
+
+        var imgMovieImage = document.createElement("img");
+        imgMovieImage.setAttribute("src",poster);
+
+        var h3MovieTitle = document.createElement("h3");
+        h3MovieTitle.className = "title is-6";
+        h3MovieTitle.textContent = movieTitle;
+
+        divInsideBox.appendChild(imgMovieImage);
+        divInsideBox.appendChild(h3MovieTitle);
+        divOutBox.appendChild(divInsideBox);
+
+        searchMovieDivContainerColumn.appendChild(divOutBox);
+    }
+    searchMovieDivContainer.appendChild(searchMovieDivContainerColumn);
+    sectionMovieResult.appendChild(searchMovieDivContainer);
+    document.body.appendChild(sectionMovieResult);
 }
 
 function calculateTotalPages(totalMovies){
